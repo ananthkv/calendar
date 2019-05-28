4151ef33870574df338583eb85986f6f72d15ee3
ananthkv <Ananth_Venugopal@amat.com>
2019-05-28 16:47:38 +0530

    'Commit-11885'

--- calendar.bat
+++ calendar.bat
@@ -1,6 +1,12 @@
 @echo off
 
-git add .
+cd C:\Ananth\Projects\git\Calendar
+
+set PATH=%PATH%;C:\Ananth\Programs\Git\bin
+
+git add SampleCalendar.ics calendar.bat readme.md
 git commit -m 'Commit-%RANDOM%'
 git push
+
+pause
 
